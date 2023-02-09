8af3b0160fdaeaf0b1f5e9eb011548f818d8c64a
Ernest <ernest.cheruiyot@student.moringaschool.com>
2023-02-09 12:27:13 +0300

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,21 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| "#{call.capitalize}!" }
+end
+
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
+end
+
+def find_the_cheese(strings)
+  cheese_types = %w[cheddar gouda camembert]
+  strings.find { |string| cheese_types.include?(string) }
+end
